c7b5c314fb3b750b554c18dd495440c432de4cd4
cafe51 <cafecafe51@hotmail.com>
2023-01-15 20:54:05 -0400

    requisito 19 resolvido

---
+++ challenges/desafio19.js
@@ -0,0 +1,25 @@
+db.produtos.updateMany(
+  {
+    // nome: { $in: ["Big Mac", "Quarteirão com Queijo"] },
+  },
+  {
+    $pull: {
+      ingredientes: "cebola",
+    },
+  },
+);
+
+db.produtos.find(
+  {
+    // ultimaModificacao: { $exists: true },
+    // tags: { $in: ['bovino', 'carne']}
+  },
+  {
+    _id: 0,
+    nome: 1,
+    ingredientes: 1,
+    // ultimaModificacao: 1,
+    // tags: 1,
+    // avaliacao: 1,
+  },
+);
